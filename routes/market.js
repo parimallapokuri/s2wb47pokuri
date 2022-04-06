c4a3a92829f10555cc015bc011ebe965f017cffd
parimallapokuri <pokuriparimalla2000@gmail.com>
2022-04-06 13:58:17 -0500

    edit

--- routes/market.js
+++ routes/market.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('market', { title: 'Search Results' });
+  res.render('market', { title: 'Search Results Market' });
 });
 
 module.exports = router;
