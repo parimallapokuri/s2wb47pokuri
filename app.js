6412be6be5ebcc3058e355678ce19982bc232a09
parimallapokuri <pokuriparimalla2000@gmail.com>
2022-04-06 12:01:40 -0500

    adding images

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var marketRouter = require('./routes/market');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/market', marketRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
